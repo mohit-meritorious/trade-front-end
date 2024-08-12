87f71ea04639853ba3b3735a9e38f9972c947d63
mohit-meritorious <mohit.meritorious@gmail.com>
2024-08-12 18:43:35 +0530

    stock detail tab initated

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,30 +1,30 @@
 import classNames from "classnames";
-import React, { useState } from "react";
+import React, { Children } from "react";
 
-export default function Tabs({ tabs, handleTabChange }) {
-  const [activeTab, setActiveTab] = useState(0);
-
+export default function Tabs({ tabs, activeTab, handleTabChange, children }) {
   return (
-    <ul className="flex flex-wrap mt-5 text-sm font-medium text-center text-gray-500 border-b border-gray-100 dark:border-gray-700 dark:text-gray-400">
-      {tabs.map((tab, i) => (
-        <li
-          key={tab.name}
-          className={classNames(
-            "me-2 border-b transition-all duration-300 p-3 dark:bg-gray-800 dark:text-primary-500 cursor-pointer",
-            {
-              "border-primary-500 bg-primary-100 text-primary-500":
-                i === activeTab,
-              "border-transparent": i !== activeTab,
-            }
-          )}
-          onClick={() => {
-            setActiveTab(i);
-            handleTabChange(i);
-          }}
-        >
-          {tab.name}
-        </li>
-      ))}
-    </ul>
+    <div className="space-y-4">
+      <ul className="flex flex-wrap mt-5 text-sm font-medium text-center text-gray-500 border-b border-gray-100 dark:border-gray-700 dark:text-gray-400">
+        {tabs.map((tab, i) => (
+          <li
+            key={tab.name}
+            className={classNames(
+              "me-2 border-b transition-all duration-300 p-3 dark:bg-gray-800 dark:text-primary-500 cursor-pointer",
+              {
+                "border-primary-500 bg-primary-100 text-primary-500":
+                  i === activeTab,
+                "border-transparent": i !== activeTab,
+              }
+            )}
+            onClick={() => {
+              handleTabChange(i);
+            }}
+          >
+            {tab.name}
+          </li>
+        ))}
+      </ul>
+      <div>{children}</div>
+    </div>
   );
 }
--- src/pages/StockDetail.jsx
+++ src/pages/StockDetail.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Breadcrumb from "../components/Breadcrumb";
 import { useParams } from "react-router-dom";
 import { firstLetterCapital } from "../utils/string";
 import StockDetailChart from "../components/stock/StockDetailChart";
 import StockOrder from "../components/stock/StockOrder";
+import Tabs from "../components/Tabs";
 export default function StockDetail() {
   const param = useParams();
   const stockName = param.stock_name
@@ -16,12 +17,34 @@ export default function StockDetail() {
     { name: "Stocks", link: "/stocks/explore" },
     { name: stockName, link: "/stocks/explore" },
   ];
+  const [activeTab, setActiveTab] = useState(0);
+
+  const TABS = [
+    { name: "Overview", value: "overview" },
+    { name: "News", value: "news" },
+    { name: "Events", value: "events" },
+    { name: "F&O", value: "fno" },
+  ];
   return (
     <div className="space-y-4">
       <Breadcrumb links={BREADCRUMB} />
       <div className="flex space-y-4">
         <div className="w-2/3">
           <StockDetailChart companyName={stockName} />
+          <Tabs
+            activeTab={activeTab}
+            handleTabChange={setActiveTab}
+            tabs={TABS}
+          >
+            {TABS[activeTab].value === "overview" && (
+              <div>Overview Tab Content</div>
+            )}
+            {TABS[activeTab].value === "news" && <div>News Tab Content</div>}
+            {TABS[activeTab].value === "events" && (
+              <div>Events Tab Content</div>
+            )}
+            {TABS[activeTab].value === "fno" && <div>F&O Tab Content</div>}
+          </Tabs>
         </div>
         <div className="w-1/3">
           <StockOrder />
