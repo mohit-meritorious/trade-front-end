6084611bd11863619480368a92551bb6f5fa7d95
mohit-meritorious <mohit.meritorious@gmail.com>
2024-08-13 16:16:49 +0530

    analyst completed

--- src/components/stock/tabs/OverviewTab/AnalystEstimates/index.jsx
+++ src/components/stock/tabs/OverviewTab/AnalystEstimates/index.jsx
@@ -5,8 +5,13 @@ import useModal, { MODAL_TYPES } from "../../../../../hooks/store/useModal";
 
 export default function AnalystEstimates() {
   const { openModal } = useModal();
+  const ANALYST = [
+    { name: "Buy", value: 78 },
+    { name: "Hold", value: 12 },
+    { name: "Sell", value: 9 },
+  ];
   return (
-    <div>
+    <div className="space-y-4">
       <div className="flex items-center space-x-1">
         <Heading3 heading={"Analyst Estimates"} />
         <button
@@ -16,6 +21,32 @@ export default function AnalystEstimates() {
         >
           <InfoIcon />
         </button>
+      </div>
+
+      <div className="flex space-x-6">
+        <div className="shrink">
+          <div className="w-32 text-3xl text-primary-500 align-middle rounded-full h-32 bg-primary-100 leading-[128px] text-center">
+            <span>78</span>
+            <span className="text-base leading-[128px]">%</span>
+          </div>
+        </div>
+        <div className="flex flex-col justify-between grow">
+          {ANALYST.map((analyst) => (
+            <div className="flex items-center space-x-4" key={analyst.name}>
+              <p className="inline-block w-10 text-sm shrink-0">
+                {analyst.name}
+              </p>
+              <span
+                className="inline-block h-2  rounded-[2px] bg-primary-500"
+                style={{ width: analyst.value + "%" }}
+              ></span>
+              <span className="text-sm">{analyst.value}%</span>
+            </div>
+          ))}
+          <div class="text-gray-500 text-sm">
+            Aggregated by Refinitiv from 32 Analysts
+          </div>
+        </div>
       </div>
     </div>
   );
