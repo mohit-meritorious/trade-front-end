79e57c84ff900130a00b97e0a98c8f3680e52ad2
mohit-meritorious <mohit.meritorious@gmail.com>
2024-08-13 11:48:32 +0530

    unnecessary code removed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import Modal from "./ui/Modal";
-import classNames from "classnames";
-import useModal from "../hooks/store/useModal";
 
 export default function Layout() {
-  const { open } = useModal();
-
   return (
     <>
       <Header></Header>
