75202958006624e14cbd82e9483f9812a68c3d16
mohit-meritorious <mohit.meritorious@gmail.com>
2024-08-20 13:21:43 +0530

    open interest stat created

--- src/components/Indieces/OpenInterest.jsx
+++ src/components/Indieces/OpenInterest.jsx
@@ -2,20 +2,42 @@ import React from "react";
 import Heading3 from "../ui/Heading3";
 import useModal, { MODAL_TYPES } from "../../hooks/store/useModal";
 import InfoIcon from "../Icon/InfoIcon";
+import { formatNumberWithCommas } from "../../utils/number";
 
 export default function OpenInterest() {
   const { openModal } = useModal();
 
   return (
-    <div className="flex items-center space-x-1">
-      <Heading3 heading={"Open Interest (OI)"} />
-      <button
-        onClick={() => {
-          openModal(MODAL_TYPES.OPEN_INTEREST);
-        }}
-      >
-        <InfoIcon />
-      </button>
+    <div className="space-y-4 ">
+      <div className="flex space-x-2">
+        <Heading3 heading={"Open Interest (OI)"} />
+
+        <button
+          onClick={() => {
+            openModal(MODAL_TYPES.OPEN_INTEREST);
+          }}
+        >
+          <InfoIcon />
+        </button>
+      </div>
+      <div className="flex justify-between">
+        <div className="space-y-2">
+          <div className="text-xs">Total Put OI</div>
+          <div className="font-semibold ">
+            {formatNumberWithCommas(4032140, false)}
+          </div>
+        </div>
+        <div className="space-y-2">
+          <div className="text-xs">Put:Call ratio</div>
+          <div className="font-semibold ">{1.16}</div>
+        </div>
+        <div className="space-y-2">
+          <div className="text-xs">Total Call OI</div>
+          <div className="font-semibold ">
+            {formatNumberWithCommas(3467426, false)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
